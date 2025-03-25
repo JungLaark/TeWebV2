6005d34f70265208311421960dbc7a08b6151419
JungLaark <chenone839@gmail.com>
2025-03-25 18:00:19 +0900

    [TeWeb] Modify Navbar
    1. Modify navbar ui

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
-import { TagItem } from '../../types';
-import { Bookmark, Tag } from 'lucide-react';
+import { Tag } from 'lucide-react';
 import TagList from '../../Objects/TagList';
 import './Navbar.css';
 
-export const Navbar: React.FC = () => {
+interface NavbarProps {
+  onSelectTag: (tag: { name: string; width: number; height: number }) => void;
+  selectedTag?: string;
+}
+
+export const Navbar: React.FC<NavbarProps> = ({ onSelectTag, selectedTag }) => {
   return (
     <div className="navbar">
       <div className="navbar-header">
-        <h2>
-          <Tag className="tag-icon" />
-          Tags
+        <h2 className="flex items-center gap-2 mb-4">
+          <Tag className="w-5 h-5" />
+          <span>Tag List</span>
         </h2>
-        <TagList 
-          onSelectTag={handleTagSelect} 
-          selectedTag={selectedTag?.name}
-        />
       </div>
+      <TagList 
+        onSelectTag={onSelectTag}
+        selectedTag={selectedTag}
+      />
     </div>
   );
 };
