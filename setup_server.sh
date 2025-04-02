5e42c8a84d57497cdc30c25aa9a9d803ee06c6b8
JungLaark <chenone839@gmail.com>
2025-04-02 15:45:47 +0900

    Fix line ending for .gitlab-ci.yml

--- setup_server.sh
+++ setup_server.sh
@@ -3,10 +3,12 @@
 echo "Setting up Node.js environment variables..."
 echo 'export PATH=/esl/TeWeb/node-v18.20.8/bin/:$PATH' >> ~/.bashrc
 source ~/.bashrc
+
 echo "Node.js environment variables set."
 echo "Updating permissions for Node.js binaries..."
 chmod +x /esl/TeWeb/node-v18.20.8/bin/node
 chmod +x /esl/TeWeb/node-v18.20.8/bin/npm
+
 echo "Permissions updated."
 echo "Copying libatomic.so.1.2.0 to /usr/lib/..."
 cp /esl/TeWeb/libatomic.so.1.2.0 /usr/lib/libatomic.so.1.2.0
