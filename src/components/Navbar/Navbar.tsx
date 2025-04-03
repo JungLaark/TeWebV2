fab574215b8e87f5e801ac7f235515ed11837132
JungLaark <chenone839@gmail.com>
2025-04-03 10:58:21 +0900

    [TeWeb][Navbar] Modify UI of scrollbar
    1. Modify UI of scrollbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import { TagItem } from '../../types';
 import { Bookmark, Tag } from 'lucide-react';
 import TagList from '../../Objects/TagList';
-import './Navbar.css';
+
 
 // ...existing code...
