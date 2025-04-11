9202eca678b36d3e75c86fdb44bda882693ec4c5
JungLaark <chenone839@gmail.com>
2025-04-11 10:28:50 +0900

    [DevSetting] Add a vite.config.ts file
    1. Add a vite.config.ts file for debuging

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
+  build: {
+    sourcemap: true, // Enable source maps for easier debugging
+  },
+  server: {
+    port: 5173, // Default port for Vite
+    },
+    optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
